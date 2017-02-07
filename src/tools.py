583e79de66e18ab51f79e0c3dd7ad168f4392638
alanxoc3 <alanxoc3@gmail.com>
2017-02-06 18:18:07 -0700

    Organized tons of code.

---
+++ src/tools.py
@@ -0,0 +1,18 @@
+import pygame as pg
+
+def split_sheet(sheet, size, columns, rows):
+    """
+    Divide a loaded sprite sheet into subsurfaces.
+    
+    The argument size is the width and height of each frame (w,h)
+    columns and rows are the integer number of cells horizontally and
+    vertically.
+    """
+    subsurfaces = []
+    for y in range(rows):
+        row = []
+        for x in range(columns): 
+            rect = pg.Rect((x*size[0], y*size[1]), size)
+            row.append(sheet.subsurface(rect))
+        subsurfaces.append(row)
+    return subsurfaces
